21a81e6d571c88f7d35a8f3271a8c13988b9c1cf
doughnuty <malika240502@gmail.com>
2021-04-11 15:27:11 +0600

    func explanations

--- model.go
+++ model.go
@@ -3,7 +3,7 @@ package main
 import "database/sql"
 
 type user struct {
-	score       int	`json: "score"`
+	score       int            `json: "score"`
 	in_progress map[string]int `json: "in_progress"`
 }
 
@@ -16,7 +16,7 @@ type record struct {
 	user      string `json: "user"`
 	residence string `json: "residence"`
 	class     string `json: "type"`
-	amount    int `json: "amount"`
+	amount    int    `json: "amount"`
 }
 
 type residence struct {
@@ -25,16 +25,22 @@ type residence struct {
 }
 
 func (u *user) dbGetUserScore(db *sql.DB, uid string) error {
-	rows, err := 
+	// select amount from history left join users where user.name=$1 and status=1
+	// select amount from history left join users where user.name=$1 and status=2
+	// select amount from history left join users where user.name=$1 and status=3
+	// add to map in progress each with key type name and amount as value
+	// sum all the values and add them to score
+
 	return err
 }
 
 func (i *item) dbGetItemByCode(db *sql.DB) error {
-
+	// insert into types if conflict select from its type id
+	// or check if type empty: y - select, n - insert
 }
 
 func (r *record) dbCreateRecord(db *sql.DB) error {
-
+	// insert into history all data
 }
 
 func dbCompareCoords(db *sql.DB, coords string) error {
